477a18a0bc0a0cc40dad912e851abe2b3ea6bd89
blumj808 <blumenfeld.jake@gmail.com>
2023-11-24 16:27:21 +0000

    save

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -14,12 +14,9 @@ class Comment < ApplicationRecord
   validates(:commenter, { :presence => true })
 
   def commenter
-    my_id = self.id
-    
+    my_id = self.author_id
     matching_users = User.where({ :id => my_id })
-
     the_user = matching_users.at(0)
-    
     return the_user
   end
 end
